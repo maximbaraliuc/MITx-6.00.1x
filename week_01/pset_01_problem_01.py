e9110ab7a9e32096cf34e03bea2581b283e95a45
maximbaraliuc <m.baraliuc@gmail.com>
2022-06-05 00:48:39 +0200

    Clean up

--- week_01/pset_01_problem_01.py
+++ week_01/pset_01_problem_01.py
@@ -3,7 +3,8 @@ Assume "s" is a string of lower case characters.
 Write a program that counts up the number of vowels contained in the string "s". Valid vowels are: "a", "e", "i", "o", and "u". For example, if s = "azcbobobegghakl", your program should print:
 Number of vowels: 5 """
 
-s = input("Enter some text...")
+s = "azcbobobegghakl"
+# ====================================================================================================================
 
 vowels_counter = 0
 for char in s:
--- week_01/pset_01_problem_02.py
+++ week_01/pset_01_problem_02.py
@@ -3,10 +3,8 @@ Assume "s" is a string of lower case characters.
 Write a program that prints the number of times the string "bob" occurs in "s". For example, if s = "azcbobobegghakl", then your program should print
 Number of times bob occurs is: 2 """
 
-# The s and "bob" are already declared in the problem "grader" on edx webplatform.
-
 s = "azcbobobegghakl"
-test_str = "bob"
+# ====================================================================================================================
 
 counter = 0
 for i in range(len(s)):
